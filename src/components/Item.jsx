16fd4a941628d5b90b603640dc9462d6281d4209
Zafer Gok <zfrgk96@gmail.com>
2022-06-22 20:23:04 +0300

    fixed css bugs, html structure and fatured css

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,35 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { NewsContext } from "../contexts/NewsContext";
+import { AiFillEye } from "react-icons/ai";
 import "../assets/scss/item.scss";
 
 function Item({ item }) {
-  const { setDetail, tesla, handleClick, detail } = useContext(NewsContext);
+  const { tesla, handleClick, detail } = useContext(NewsContext);
+  const [items, setItems] = useState(item);
+
   let splitUrl = item.title.toLowerCase();
   splitUrl = splitUrl.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
   let newUrl = splitUrl.replace(/\s/g, "-");
-
-  console.log("detail", detail);
 
   return (
     <div className="col-md-3 card-wrapper">
       <div className="card">
         <div className="card-header">
           <img
-            src={item.urlToImage}
-            alt={item.title}
+            src={items.urlToImage}
+            alt={items.title}
             className="card-header-image"
           />
         </div>
         <div className="card-body">
-          <h5 className="card-body-title">{item.title}</h5>
-          <p className="card-body-text">{item.description}</p>
+          <h5 className="card-body-title">{items.title}</h5>
+          <p className="card-body-text">{items.description}</p>
         </div>
-        <div className="card-footer">
+        <div className={`card-footer end`}>
           <a
             onClick={() => {
               handleClick(item, newUrl);
             }}
-            className="btn btn-primary"
+            className="card-footer-readMore"
           >
             Read More
           </a>
