c3b4c2b56854e84c6c7bba8d1283f94da08150ef
Zafer Gok <zfrgk96@gmail.com>
2022-06-22 20:16:15 +0300

    created `Footer` component and `footer`.scss file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../assets/scss/footer.scss";
+import { BsGithub } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <p>
+          You can access the source codes of this website on
+          <BsGithub
+            className="github-icon"
+            onClick={() => {
+              window.open("https://github.com/zzafergok/sample-todo-react-app");
+            }}
+          />
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
