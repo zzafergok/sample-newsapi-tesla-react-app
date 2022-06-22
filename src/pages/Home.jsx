7cfcf28d9e5eef176e879a787de389b09f43e765
Zafer Gok <zfrgk96@gmail.com>
2022-06-22 20:21:06 +0300

    added `Navbar & Footer` components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ListItem from "../components/ListItem";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <ListItem />
+      <Footer />
     </div>
   );
 };
