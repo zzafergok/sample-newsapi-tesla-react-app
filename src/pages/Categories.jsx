3db109737b9ab3d57250ea188d2f59613b4e0b43
Zafer Gok <zfrgk96@gmail.com>
2022-06-23 23:06:43 +0300

    added remove local storage function

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListItem from "../components/ListItem";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Category from "../components/Category";
 
 function Categories() {
+  useEffect(() => {
+    localStorage.removeItem("detail");
+    localStorage.removeItem("searchDetail");
+  }, []);
   return (
     <div>
       <Navbar />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListItem from "../components/ListItem";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Category from "../components/Category";
 
 const Home = () => {
+  useEffect(() => {
+    localStorage.removeItem("detail");
+    localStorage.removeItem("searchDetail");
+  }, []);
   return (
     <div>
       <Navbar />
