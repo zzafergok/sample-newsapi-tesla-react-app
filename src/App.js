86a1f05f41ddc0924b2ec18ac3018741904ff87d
Zafer Gok <zfrgk96@gmail.com>
2022-06-23 03:42:29 +0300

    created `category & sort` routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NewsContext } from "./contexts/NewsContext";
 import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import Categories from "./pages/Categories";
 
 const App = () => {
+  const { allSort, allCategory } = useContext(NewsContext);
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/:id" element={<Details />} />
+        {allCategory.map((category) => {
+          return (
+            <Route
+              key={category}
+              path={`/${category}`}
+              element={<Categories />}
+            />
+          );
+        })}
+
+        {allCategory.map((category) => {
+          return allSort.map((sort) => {
+            return (
+              <Route
+                key={sort}
+                path={`/${category}?sortBy=${sort}`}
+                element={<Categories />}
+              />
+            );
+          });
+        })}
       </Routes>
     </>
   );
