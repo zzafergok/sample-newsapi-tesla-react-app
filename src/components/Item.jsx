ac4602137f221813c89b88339146a966566fe453
Zafer Gok <zfrgk96@gmail.com>
2022-06-23 03:30:59 +0300

    created `getCategory` function

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,13 +7,40 @@ function Item({ item }) {
   const { tesla, handleClick, detail } = useContext(NewsContext);
   const [items, setItems] = useState(item);
 
-  let splitUrl = item.title.toLowerCase();
-  splitUrl = splitUrl.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-  let newUrl = splitUrl.replace(/\s/g, "-");
+  let splitUrl = item && item.title && item.title.toLowerCase();
+  splitUrl = splitUrl && splitUrl.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  let newUrl = splitUrl && splitUrl.replace(/\s/g, "-");
+
+  const getCategory = (imageUrl, title, desc) => {
+    return (
+      <div className="card">
+        <div>
+          <div className="card-header">
+            <img src={imageUrl} alt={title} className="card-header-image" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-body-title">{title}</h4>
+            <p className="card-body-text">{desc}</p>
+          </div>
+        </div>
+        <div className={`card-footer end`}>
+          <a
+            onClick={() => {
+              handleClick(item, newUrl);
+            }}
+            className="card-footer-readMore"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="col-md-3 card-wrapper">
-      <div className="card">
+      {getCategory(items.urlToImage, items.title, items.description)}
+      {/* <div className="card">
         <div className="card-header">
           <img
             src={items.urlToImage}
@@ -35,7 +62,7 @@ function Item({ item }) {
             Read More
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
