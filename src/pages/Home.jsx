09d9b17bb17fe992f7accd47fb34ed3b2f70b519
Zafer Gok <zfrgk96@gmail.com>
2022-06-23 03:34:47 +0300

    added `Sort & Category` components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ListItem from "../components/ListItem";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Category from "../components/Category";
+import Sort from "../components/Sort";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Category />
+      <Sort />
       <ListItem />
       <Footer />
     </div>
