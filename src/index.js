a500699d12692b4553aace303da630384ee5da41
Zafer Gok <zfrgk96@gmail.com>
2022-06-23 21:47:58 +0300

    added `react-loader-spinner` dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/scss/index.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
