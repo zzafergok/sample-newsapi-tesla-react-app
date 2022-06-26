95c82579f53f95ebfd75fa04643375aa05de03e5
Zafer Gok <zfrgk96@gmail.com>
2022-06-26 04:03:49 +0300

    update code lines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/scss/index.scss";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import "./assets/scss/index.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 import { BrowserRouter } from "react-router-dom";
 import NewsContextProvider from "./contexts/NewsContext";
 
